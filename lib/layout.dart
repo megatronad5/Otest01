cb3e015ea7012529ceabe252268c4d2f57909b4a
megatrond5 <megatron.ad5@gmail.com>
2024-06-17 03:13:10 +0200

    continue

---
+++ lib/layout.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SiteLayout extends StatelessWidget {
+  const SiteLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+      body: Row(
+        children: [],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:administration_ivgaz/layout.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -17,8 +18,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           scaffoldBackgroundColor: Colors.white,
           textTheme: GoogleFonts.mulishTextTheme(Theme.of(context).textTheme)
-              .apply(bodyColor: Colors.black)),
-      home: Container(),
+              .apply(bodyColor: Colors.black),
+          pageTransitionsTheme: const PageTransitionsTheme(builders: {
+            TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
+            TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+          }),
+          primaryColor: Colors.blue),
+      home: const SiteLayout(),
     );
   }
 }
